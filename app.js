f2ea95f9b77d3287ee0f789e1ca25957b5852690
Abdul Karim <Abdul127001@users.noreply.github.com>
2021-01-15 16:59:28 +0530

    Adding Styles

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var txtInput = document.querySelector("#txtInput");
 var output = document.querySelector("#output");
 
 //var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-//var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 //var serverURL = "https://api.funtranslations.com/translate/pirate.json";
-var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+//var serverURL = "https://api.funtranslations.com/translate/yoda.json";
 
 function getTranslationUrl(txt) {
     return serverURL + "?" + "text=" + txt;
